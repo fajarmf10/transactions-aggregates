4a19b48cc44c8dfc9c1520ccfd1a3e10eefb348c
fajarmf10 <fajarmf78@gmail.com>
2026-05-23 19:21:23 +0700

    feat: add .env.example file and integrate godotenv for environment variable management

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type config struct {
 	databaseURL string
@@ -8,6 +13,10 @@ type config struct {
 }
 
 func loadConfig() config {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("note: .env not loaded (%v); using existing environment", err)
+	}
+
 	addr := os.Getenv("LISTEN_ADDR")
 	if addr == "" {
 		addr = ":8080"
